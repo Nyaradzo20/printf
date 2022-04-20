f4f7b284aa5efd956dc058399e8ec5d3fc4cbf25
Nyaradzo20 <nchinyati20@gmail.com>
2022-04-20 06:51:40 -0700

    pracs

---
+++ main.h
@@ -0,0 +1,13 @@
+#define MAIN_H
+#ifndef MAIN_H
+#include <unistd.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+int _printf(const char *format, ...);
+int _putchar(char c)
+  
+#endif
+/*MAIN_H*/
