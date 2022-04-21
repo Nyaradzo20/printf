52e16574f3ac51cf86f747f0cfaf082838a6ba7e
Nyaradzo20 <nchinyati20@gmail.com>
2022-04-21 03:42:40 -0700

    code has 4 errors

--- main.h
+++ main.h
@@ -1,13 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <unistd.h>
-#include <limits.h>
-#include <stdio.h>
 #include <stdarg.h>
 #include <stdlib.h>
-#include <string.h>
-int _printf(const char *format, ...);
+#include <unistd.h>
+int _printf(char *format, ...);
 int _putchar(char c);
-  
+int _puts(char *s);
+
 #endif
-/*MAIN_H*/
